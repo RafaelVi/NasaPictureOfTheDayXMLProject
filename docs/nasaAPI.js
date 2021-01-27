bc4739d7ad087a66e393c8f71e2d4154c2aa372c
RafaelVi <rafaelalmeida383@gmail.com>
2021-01-27 16:43:34 -0300

    git pages update

--- docs/nasaAPI.js
+++ docs/nasaAPI.js
@@ -1,3 +1,4 @@
+
 class nasaModel  {
     constructor(){
         this._descricao = "";
@@ -49,7 +50,12 @@ class nasaViews {
         //Crio todos os meus elementos
         this._data = document.createElement("p");
         this._descricao = document.createElement("p");
-        this._img = document.createElement("img");
+        if(model.img.match(/\.(jpg|gif|png)$/)!= null){//verifica se o link é uma imagem
+            this._img = document.createElement("img");
+        }
+        else{
+            this._img = document.createElement("iframe");
+        }
         //Atribuo os dados do model aos meus elementos
         this._img.src = model.img;
         this._data.innerText = model.data;
@@ -86,61 +92,74 @@ class nasaController {
         views.criarElementos();//Joga os elementos na tela
 
     }
-    verificaData(data,operador)
-    {
+    verificaData(input,data,operador)
+    {   
+        
         //Pego a data e adiciono a hora como meia noite para que não interfira
         let auxDate = new Date(data+"T00:00:00");
-        //Verifico se vai ir pra dia anterior ou posterior
-        if(operador == "+"){
-            if(auxDate.getTime() == new Date("Jun 16 1995").getTime()){
-                //Correção de bug: os dias 17,18 e 19 não possuem dados
-                //Pulo 4 dias: do dia 16 para o 20
-                auxDate.setDate(auxDate.getDate() + 4);
-            }
-            else{
-                auxDate.setDate(auxDate.getDate() + 1);//Se for posterior acrescenta um dia
-                if(auxDate > Date.now()){
-                    alert("Você não pode buscar imagens de dias futuros");
-                    auxDate = new Date();
-                    
-                }
-            }
-        }
-        else if(operador == "-"){
-            
-            if(auxDate.getTime() == new Date("Jun 20 1995").getTime()){
-                //Correção de bug: os dias 17,18 e 19 não possuem dados
-                //Pulo 4 dias: do dia 20 para o 16
-                auxDate.setDate(auxDate.getDate() - 4);
-            }
-            else{
-                auxDate.setDate(auxDate.getDate() - 1);//Se for anterior tira um dia
 
-                if(auxDate < new Date("Jun 16 1995")){//Se for menor que a data minima volta para a data minima
-                    alert("A data minima é 16/06/1995");
-                    auxDate.setDate(auxDate.getDate() + 1);
+        
+        
+
+            //Verifico se vai ir pra dia anterior ou posterior
+            if(operador == "+"){
+                if(auxDate.getTime() == new Date("Jun 16 1995").getTime()){
+                    //Correção de bug: os dias 17,18 e 19 não possuem dados
+                    //Pulo 4 dias: do dia 16 para o 20
+                    auxDate.setDate(auxDate.getDate() + 4);
                 }
-            }    
-            
-        }
-        else{
-            if(auxDate > Date.now()){
-                //Se for maior que o dia atual (now()) volta para o dia atual
-                alert("Você  não pode buscar imagens de dias futuros");
-                auxDate = new Date();
+                else{
+                    auxDate.setDate(auxDate.getDate() + 1);//Se for posterior acrescenta um dia
+                    if(auxDate > Date.now()){
+                        alert("Você não pode buscar imagens de dias futuros");
+                        auxDate = new Date(data+"T00:00:00");
+                        
+                    }
+                }
             }
-            if(auxDate < new Date("Jun 16 1995")){
-                //Se for menor que a data minima volta para o dial atual
-                alert("A data minima é 16/06/1995");
-                auxDate = new Date();
+            else if(operador == "-"){
+                
+                if(auxDate.getTime() == new Date("Jun 20 1995").getTime()){
+                    //Correção de bug: os dias 17,18 e 19 não possuem dados
+                    //Pulo 4 dias: do dia 20 para o 16
+                    auxDate.setDate(auxDate.getDate() - 4);
+                }
+                else{
+                    auxDate.setDate(auxDate.getDate() - 1);//Se for anterior tira um dia
+
+                    if(auxDate < new Date("Jun 16 1995")){//Se for menor que a data minima volta para a data minima
+                        alert("A data minima é 16/06/1995");
+                        auxDate.setDate(auxDate.getDate() + 1);
+                    }
+                }    
+                
             }
-            if(auxDate.getTime() == new Date("Jun 17 1995").getTime() || auxDate.getTime() == new Date("Jun 18 1995").getTime() || auxDate.getTime() == new Date("Jun 19 1995").getTime()){
-                //Se for os dias 17,18 e 19 de junho de 1995. Avisa que não é possivel ver este dia e volta para o dia atual
-                alert("Parabéns, você chegou no limbo! essa data não esta nosso banco de dados");
-                auxDate = new Date();
+            else{
+                auxDate = new Date(input.value + "T00:00:00");
+                if(input.validity.badInput)//verifico se a data inserida é invalida
+                {
+                    alert(input.validationMessage);//se for disparo uma mensagem de erro e retorno para a 
+                    auxDate =  new Date(data+"T00:00:00");   
+                }
+                else{
+                    if(auxDate > Date.now()){
+                        //Se for maior que o dia atual (now()) volta para o dia atual
+                        alert("Você  não pode buscar imagens de dias futuros");
+                        auxDate = new Date(data+"T00:00:00");
+                    }
+                    if(auxDate < new Date("Jun 16 1995")){
+                        //Se for menor que a data minima volta para o dial atual
+                        alert("A data minima é 16/06/1995");
+                        auxDate = new Date(data+"T00:00:00");
+                    }
+                    if(auxDate.getTime() == new Date("Jun 17 1995").getTime() || auxDate.getTime() == new Date("Jun 18 1995").getTime() || auxDate.getTime() == new Date("Jun 19 1995").getTime()){
+                        //Se for os dias 17,18 e 19 de junho de 1995. Avisa que não é possivel ver este dia e volta para o dia atual
+                        alert("Parabéns, você chegou no limbo! essa data não esta nosso banco de dados");
+                        auxDate = new Date();
+                    }
+                }
+                
             }
-            
-        }
         //Pego essa nova data e converto para YYYY-MM-DD
         let ArrayData = auxDate.toISOString();
         // A string vai ficar mais ou menos assim: "2002-03-14T00:00:00"
@@ -148,7 +167,7 @@ class nasaController {
         //Eu só quero a data, ent divido essa string em duas partes: "2002-03-14" e "00:00:00" usando o split pra dividir pelo T
         return auxDate2[0];
     }
-
+    
     
 }
 // Pega meus inputs: data e os 3 botões
@@ -179,8 +198,10 @@ diaAtual = yyyy+'-'+mm+'-'+dd;
 window.onload = nasa.buscaImagem(diaAtual);
 // Adicionando o evento de buscar um dia específico, também remove o dia que estava sendo mostrado
 buttonDiaEspecifico.addEventListener("click",function(){
+    let diaAtual = document.getElementById("data");//pega o paragrafo do meu dia atual
+    diaAtual = diaAtual.textContent;
     document.getElementById("divContainer").remove();
-    let data = nasa.verificaData(date.value,"");
+    let data = nasa.verificaData(date,diaAtual,"");
     date.value = "";//limpa meu input data
     nasa.buscaImagem(data);//adiciona o dia especifico na tela
      
@@ -189,19 +210,21 @@ buttonDiaEspecifico.addEventListener("click",function(){
 buttonDiaPosterior.addEventListener("click",function(){
     let diaAtual = document.getElementById("data");//pega o paragrafo do meu dia atual
     diaAtual = diaAtual.textContent;//pega o texto desse paragrafo
-    let novoDia = nasa.verificaData(diaAtual,"+");//verifico o dia e retorno o dia posterior
+    let novoDia = nasa.verificaData(date,diaAtual,"+");//verifico o dia e retorno o dia posterior
     document.getElementById("divContainer").remove();//removo o dia anterior
     
-     nasa.buscaImagem(novoDia);//adiciono o proximo dia;
+    nasa.buscaImagem(novoDia);//adiciono o proximo dia;
      
 });
 // Busca o dia anterior e remove o dia atual
 buttonDiaAnterior.addEventListener("click",function(){
     let diaAtual = document.getElementById("data");
     diaAtual = diaAtual.textContent;
-    let novoDia = nasa.verificaData(diaAtual,"-");//verifico o dia e retorno o dia anterior
+    let novoDia = nasa.verificaData(date,diaAtual,"-");//verifico o dia e retorno o dia anterior
     document.getElementById("divContainer").remove();
-    
-     nasa.buscaImagem(novoDia);
+    nasa.buscaImagem(novoDia);
      
 });
+console.log(`Datas para testar: 4/01/2019 - gif
+            24/04/2020 - video
+            29/02/2020 - ano bissexto`);
